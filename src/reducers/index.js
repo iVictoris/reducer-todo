9d8ffcff6855ce0f86e4d3a74190b9b5866f5aab
Victor Tran <victor.n.tran@outlook.com>
2019-11-11 11:32:48 -0800

    revised reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,21 +3,22 @@ const initialState = [
     text: "Learn about reducers",
     completed: false,
     id: 3892987589
-
   }
-]
-
+];
 
 const reducer = (initialState, action) => {
+  let todos;
   switch (action.type) {
-    case 'ADD_TODO':
-      const todos = [...initialState, action.payload];
+    case "ADD_TODO":
+      todos = [...initialState, action.payload];
       return todos;
+    case "COMPLETE_TODO":
+      const todo = initialState.filter(todo => todo.id === action.payload);
+      todos = initialState.filter(todo => todo.id !== action.payload);
+      todo.completed = !todo.completed;
+      return [...todos, todo];
     default:
       return initialState;
   }
-}
-export {
-  reducer,
-  initialState
-}
+};
+export { reducer, initialState };
