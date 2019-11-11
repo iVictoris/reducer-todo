efcc57e4d22e4ff96ed0009ecacfb2bccff0bcf1
Victor Tran <victor.n.tran@outlook.com>
2019-11-11 11:06:36 -0800

    import error with max stack trace, refrain from importing app, mapped todos

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,5 @@
 import React, { useReducer } from "react";
 
-import { App } from "./App";
-
 // * TodoForm imports
 import { Form, Field, withFormik } from "formik";
 import * as Yup from "yup";
@@ -9,36 +7,55 @@ import * as Yup from "yup";
 // * TodoList imports
 import { initialState, reducer } from "../reducers";
 
-
 // * Todo Form
 const TodoForm = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <Form>
-      <Field type="text" placeholder="Add Todo" name='todo'/>
+      <Field type="text" placeholder="Add Todo" name="todo" />
     </Form>
   );
 };
 
 const EnchancedTodoForm = withFormik({
-  mapPropsToValues ({todo = ''}) {
-    return {todo}
+  mapPropsToValues({ todo = "" }) {
+    return { todo };
   },
   validationSchema: Yup.object().shape({
-    todo: Yup.string().required('Required to input text here')
+    todo: Yup.string().required("Required to input text here")
   }),
 
-  handleSubmit({todo}, {props: {addTodo}, resetForm}) {
-    addTodo(todo)
-    resetForm({todo: ''});
+  handleSubmit(
+    { todo },
+    {
+      props: { addTodo },
+      resetForm
+    }
+  ) {
+    addTodo(todo);
+    resetForm({ todo: "" });
   }
-})(TodoForm)
+})(TodoForm);
 
 // * TodoList
 const TodoList = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const todos = state.map(item => {
+    return (<div key={item.id}>
+      <p>{item.text}</p>
+    </div>);
+  });
 
-  return <div className="TodoList"></div>;
+  return <div className="TodoList">{todos}</div>;
 };
+
+function App() {
+  return (
+    <div className="App">
+      <EnchancedTodoForm />
+      <TodoList />
+    </div>
+  );
+}
 
 export { App, TodoList, EnchancedTodoForm };
