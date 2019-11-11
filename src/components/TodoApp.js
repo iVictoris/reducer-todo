6737dcd4f6c973b94997bf2e1ef3266d5bbd0a87
Victor Tran <victor.n.tran@outlook.com>
2019-11-11 11:56:32 -0800

    remove props, add Provider and link value prop to include state and dispatch

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,18 +1,19 @@
-import React, {useContext, useReducer} from 'react';
+import React, {useReducer} from 'react';
 
 import EnchancedTodoForm from './EnchancedTodoForm';
 import TodoList from './TodoList';
 
 import {reducer, initialState} from '../reducers';
+import {TodoContext} from '../contexts';
 
 
-const TodoApp = ({children}) => {
+const TodoApp = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <>
+    <TodoContext.Provider value={{state, dispatch}}>
       <EnchancedTodoForm dispatch={dispatch} />
       <TodoList state={state} dispatch={dispatch}/>
-    </>
+    </TodoContext.Provider>
   );
 };
 
