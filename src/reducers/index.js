ffacd2ba6cecd909de2229420448f0000391d578
Victor Tran <victor.n.tran@outlook.com>
2019-11-11 10:42:32 -0800

    build out simple reducer and export

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+const initialState = {
+  item: "Learn about reducers",
+  completed: false,
+  id: 3892987589
+};
+
+
+const reducer = (initialState) => initialState
+
+export {
+  reducer,
+  initialState
+}
