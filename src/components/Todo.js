8da2189047bac25fa8ced2d0eed7626ad33ceb1f
Victor Tran <victor.n.tran@outlook.com>
2019-11-11 12:23:41 -0800

    import Context and take dispatch and dispatch a done action on click

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {TodoContext} from '../contexts';
 
 const Todo = ({id, text, completed}) => {
+  const {dispatch} = useContext(TodoContext);
+
   return (
-    <div id={id} className={'Todo'.concat(completed ? '-done' : '')}>
+    <div id={id} className={'Todo'.concat(completed ? ' done' : '')} onClick={() => dispatch({type: 'COMPLETE_TODO', payload: id})}>
         <p>{text}</p>
       </div>
   )
