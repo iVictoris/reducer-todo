d1aba844d6e8ed0c3862c7a4bfcae52a2a89f080
Victor Tran <victor.n.tran@outlook.com>
2019-11-11 11:07:16 -0800

    change reducer to have an action.type conditional with switch

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,22 @@
-const initialState = {
-  item: "Learn about reducers",
-  completed: false,
-  id: 3892987589
-};
+const initialState = [
+  {
+    text: "Learn about reducers",
+    completed: false,
+    id: 3892987589
+
+  }
+]
 
 
-const reducer = (initialState) => initialState
-
+const reducer = (initialState, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      const todos = [...initialState, action.payload];
+      return todos;
+    default:
+      return initialState;
+  }
+}
 export {
   reducer,
   initialState
