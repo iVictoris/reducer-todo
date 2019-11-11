8bd6743b39e34bb02179cc3462f2dd2ab5f0ac26
Victor Tran <victor.n.tran@outlook.com>
2019-11-11 10:55:38 -0800

    create components that will later be in their own files

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,43 @@
 import React, { useReducer } from "react";
-import { initialState, reducer } from "../reducers";
 
 import { App } from "./App";
 
+// * TodoForm imports
+import { Form, Field, withFormik } from "formik";
+import * as Yup from "yup";
+
+// * TodoList imports
+import { initialState, reducer } from "../reducers";
+
+
+// * Todo Form
+const todoForm = () => {
+  return (
+    <Form>
+      <Field type="text" placeholder="Add Todo" name='todo'/>
+    </Form>
+  );
+};
+
+const TodoForm = withFormik({
+  mapPropsToValues ({todo = ''}) {
+    return {todo}
+  },
+  validationSchema: Yup.object().shape({
+    todo: Yup.string().required('Required to input text here')
+  }),
+
+  handleSubmit({todo}, {props: {addTodo}, resetForm}) {
+    addTodo(todo)
+    resetForm({todo: ''});
+  }
+})(todoForm)
+
+// * TodoList
 const TodoList = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return <div className="TodoList"></div>;
 };
 
-export { App, TodoList };
+export { App, TodoList, TodoForm };
